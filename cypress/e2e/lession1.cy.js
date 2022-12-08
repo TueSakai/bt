7fb7e50b153b6c9c53162b9c70475845f16e46ba
CVThang <cvthang@cmcglobal.vn>
2022-12-08 15:31:16 +0700

    4th commit

--- cypress/e2e/lession1.cy.js
+++ cypress/e2e/lession1.cy.js
@@ -3,11 +3,14 @@ describe('Test case search keyword in Google and verify result', () => {
         cy.visit('https://www.google.com')
     })
 
-    it('TC01 - Search an random keyword (Google)', () => {
+    it('Search an random keyword (Google)', () => {
         
         cy.get('input[name="q"]').should('be.empty').type('google{enter}');
         //verify result
+
+        //have keyword Google
         cy.get('h3').should('include.text',"Google");
+        // link www.google.com appear
         cy.get('cite').should('include.text',"www.google.com.vn");
         
     })
