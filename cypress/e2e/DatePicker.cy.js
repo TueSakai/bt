87e393f5e88a013315db29adb67a4c2d2ab91b3a
CVThang <cvthang@cmcglobal.vn>
2022-12-28 15:22:15 +0700

    BTVN 3

--- cypress/e2e/DatePicker.cy.js
+++ cypress/e2e/DatePicker.cy.js
@@ -1,18 +1,38 @@
+import PageDatePicker from "../support/pageUI/PageDatePicker"
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
 
-describe('Date picker', () => {
 
+
+describe('Date picker', () => {
+    const pageDatePicker = new PageDatePicker()
+    let year = 2013
+    let month = 1
+    let yearNow = 2022
     beforeEach (() => {
         cy.viewport(1920,1080)
         cy.visit('https://demo.automationtesting.in/Datepicker.html')
     })
-    let i=0
+
     it('DatePicker Enable', () => {
-        cy.xpath("//input[@id='datepicker2']").click()
+        pageDatePicker.getCalendar("2").click()
+        pageDatePicker.changeDate("month").select("May")
+
+        pageDatePicker.changeDate("year").select("▲")
+        pageDatePicker.changeDate("year").select("1999")
+        pageDatePicker.changeDay("4").click()
+        cy.wait(3000)
     })
 
+    it('DatePicker Disable', () => {
+        pageDatePicker.getCalendar("1").click()
+        for (var i = 1; i < 12*(yearNow-year)+(12-month+1); i++) {
+            pageDatePicker.previousButton().click()
+        }
+        pageDatePicker.changeDay2("17").click()
+        
+    })
 
 })
---
+++ cypress/support/pageUI/PageDatePicker.js
@@ -0,0 +1,20 @@
+class PageDatePicker {
+    getCalendar(value){
+        return cy.xpath("//input[@id='datepicker"+value+"']")
+    }
+    changeDate(value){
+        return cy.xpath("//select[@title='Change the "+value+"']")
+    }
+    changeDay(value){
+        return cy.xpath("//a[text()='"+value+"']")
+    }
+    previousButton(){
+        return cy.xpath("//span[text()='Prev']")
+    }
+    changeDay2(value){
+        return cy.xpath("//td[@data-handler='selectDay']/a[text()='"+value+"']")
+    }
+
+}
+
+export default PageDatePicker
